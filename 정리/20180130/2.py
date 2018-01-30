8bb6cd63965b7fed087caee271ae493e8b6af404
ljg95924 <ljg95924@naver.com>
2018-01-30 11:14:22 +0900

    python first commit

---
+++ 정리/20180130/2.py
@@ -0,0 +1,17 @@
+import turtle
+t=turtle.Pen()
+t.right(180)
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(150)
+t.forward(100)
+t.right(150)
+t.right(90)
+t.forward(100)
+t.circle(100)
