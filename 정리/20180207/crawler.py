1693d1efbb37a27927133a4b7f223de03ea3c8c9
ljg95924 <ljg95924@naver.com>
2018-02-08 09:23:42 +0900

    0207

--- 정리/20180207/crawler.py
+++ 정리/20180207/crawler.py
@@ -1,20 +1,28 @@
 import urllib.request
 import urllib.parse
 from bs4 import BeautifulSoup
-defaultURL='http://openapi.naver.com/search?&'
-key='key='
+defaultURL='https://openapi.naver.com/v1/search/kin.xml?'
+key='key=_M6QMrUwSzIQrzLnWOMj'
 target='&target=news'
 sort='&sort=sim'
 start='&start=1'
 display='&display=100'
 query='&query='+urllib.parse.quote_plus(str(input("검색어를 입력하세요:")))
 
-fullURL=defaultURL+key+target+sort+start+display+query
+fullURL=defaultURL + sort + start + display + query
 
 print(fullURL)
 file=open('C:\\python36\\naver_news.txt','w',encoding='utf-8')
-
-f=urllib.request.urlopen(fullURL)
+headers={
+    'Host':'openapi.naver.com',
+    'User-Agent':'curl/7.43.0',
+    'Accept':'*/*',
+    'Content-Type':'application/xml',
+    'X-Naver-Client-Id':'_M6QMrUwSzIQrzLnWOMj',
+    'X-Naver-Client-Secret':'JCk88jjh7h'
+    }
+req=urllib.request.Request(fullURL,headers=headers)
+f=urllib.request.urlopen(req)
 resultXML=f.read()
 xmlsoup=BeautifulSoup(resultXML,'html.parser')
 
