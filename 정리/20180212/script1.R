a004e780883bd01f94eb7c5f6d9c3b5e3550cd8d
ljg95924 <ljg95924@naver.com>
2018-02-12 14:51:42 +0900

    R programing basic

---
+++ 정리/20180212/script1.R
@@ -0,0 +1,23 @@
+a<-11
+b<-22
+getwd()
+var1<-c(1,2,5,7,8)
+var2<-c(1:5)
+var3<-seq(1,5)
+var4<-seq(1,10, by=2)
+var1
+var2
+var3
+var4
+var1+2
+var1+var2
+str1<-'a'
+str2<-'text'
+str3<-'Hello World'
+str4<-c('a','b','c')
+str5<-c('Hello!','world','is','good!')
+paste(str5,collapse=",")
+x<-c(1,2,3)
+mean(x)
+max(x)
+min(x)
